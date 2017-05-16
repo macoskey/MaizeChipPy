d8886c37e8560c1f32413e0ad0150e6ac150942b
jsukes <jsukes@umich.edu>
2017-05-16 09:47:13 -0400

    renamed lithium back to 'v'

---
+++ MaizeChipPy/v.py
@@ -0,0 +1,21 @@
+# Loads all functions including a commands, b commands, tests, and basic steering
+# type "from varLoader import *" on startup of python using "sudo python"
+#
+# MaizeChipPy 1.0 - JJM - May 2017
+
+
+from fpgaClass import *
+from tests import *
+from treatments import *
+from pointUploader import *
+from patterns import *
+
+x = FPGA()
+a = a_funcs(x)
+b = b_funcs(x)
+
+ul = point_uploader(b)
+
+
+		
+
